af6bcb75ce58b449022f5751310cd1d80dbb12c5
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-04-29 21:00:22 -0400

    Major update
    I hope it will work fine x)

---
+++ src/javascript/region-option.js
@@ -0,0 +1,42 @@
+function detect_region() {
+    var region = document.getElementById("region"); // getting the selected server
+    var selected_region = region.options[region.selectedIndex].text;
+
+    switch (selected_region) {
+        case "North America":
+            document.getElementById("REGION").action = "https://na.op.gg/summoner/";
+            break;
+        case "Korea":
+            document.getElementById("REGION").action = "https://www.op.gg/summoner/";
+            break;
+        case "Japan":
+            document.getElementById("REGION").action = "https://jp.op.gg/summoner/";
+            break;
+        case "Europe West":
+            document.getElementById("REGION").action = "https://euw.op.gg/summoner/";
+            break;
+        case "Europe Nordic & East":
+            document.getElementById("REGION").action = "https://eune.op.gg/summoner/";
+            break;
+        case "Oceania":
+            document.getElementById("REGION").action = "https://oce.op.gg/summoner/";
+            break;
+        case "Brazil":
+            document.getElementById("REGION").action = "https://br.op.gg/summoner/";
+            break;
+        case "Latin America (South)":
+            document.getElementById("REGION").action = "https://las.op.gg/summoner/";
+            break;
+        case "Latin America (North)":
+            document.getElementById("REGION").action = "https://lan.op.gg/summoner/";
+            break;
+        case "Russia":
+            document.getElementById("REGION").action = "https://ru.op.gg/summoner/";
+            break;
+        case "Turkey":
+            document.getElementById("REGION").action = "https://tr.op.gg/summoner/";
+            break;
+        default:
+            document.getElementById("REGION").action = "https://op.gg/summoner/";
+    }
+}
