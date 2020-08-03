c367a361a458877aebb29b1ec025b5a57f6dc8fd
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-08-03 10:09:14 -0400

    Bug fix
    + Fixed unloading images

--- src/javascript/champions.js
+++ src/javascript/champions.js
@@ -3,29 +3,29 @@ async function displayChampions() {
         const data = new Request(`../src/json/champions.json`);
         const { champions } = await fetch(data).then(data => data.json());
 
-        document.getElementById("champ-toptext").innerHTML = ("With " + champions.length + " champions, you’ll find the perfect match for your playstyle. Master one, or master them all.")
+        document.getchampionById("champ-toptext").innerHTML = ("With " + champions.length + " champions, you’ll find the perfect match for your playstyle. Master one, or master them all.")
 
-        champions.slice().forEach((element, i) => {
+        champions.slice().forEach((champion, i) => {
             // I change the 'on_error_link' because I often code without internet connection - the second link should always be inactive.
-            var on_error_link = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${element.name}_0.jpg'
+            var on_error_link = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.name}_0.jpg'
             // var on_error_link = '../src/img/image_not_found.png'
-            document.getElementById(`champions`).innerHTML +=
+            document.getchampionById(`champions`).innerHTML +=
                 `<figure>
-                    <a id="get${element.name}Stats" onclick="loadChampData('${on_error_link}',
-                    '${element.name}', '${element.difficulty_rank}', 
-                    '${element.passive_name}', '${element.passive_description}',
-                    '${element.q_name}', '${element.q_description}', '${element.w_name}', '${element.w_description}',
-                    '${element.e_name}', '${element.e_description}', '${element.r_name}', '${element.r_description}',
-                    '${element.meta_tier}');">
-                    <img class="champ-img" id="get${element.name}Icon" src="../src/img/icon/${element.name}.jpg"
-                    onerror="javascript: document.getElementById('get${element.name}Icon').src = '../src/img/image_not_found.png'">
-                    <figcaption id="champ${element.name}Name"></figcaption></a>
+                    <a id="get${champion.name}Stats" onclick="loadChampData('${on_error_link}',
+                    '${champion.name}', '${champion.difficulty_rank}', 
+                    '${champion.passive_name}', '${champion.passive_description}',
+                    '${champion.q_name}', '${champion.q_description}', '${champion.w_name}', '${champion.w_description}',
+                    '${champion.e_name}', '${champion.e_description}', '${champion.r_name}', '${champion.r_description}',
+                    '${champion.meta_tier}');">
+                    <img class="champ-img" id="get${champion.name}Icon" src="../src/img/icon/${champion.name}.jpg"
+                    onerror="javascript: document.getchampionById('get${champion.name}Icon').src = ${on_error_link}">
+                    <figcaption id="champ${champion.name}Name"></figcaption></a>
                 </figure>`;
 
-            if (element.displayName) {
-                document.getElementById(`champ${element.name}Name`).innerHTML = `${element.displayName}`;
+            if (champion.displayName) {
+                document.getchampionById(`champ${champion.name}Name`).innerHTML = `${champion.displayName}`;
             } else {
-                document.getElementById(`champ${element.name}Name`).innerHTML = `${element.name}`;
+                document.getchampionById(`champ${champion.name}Name`).innerHTML = `${champion.name}`;
             }
         });
     } catch (err) {
