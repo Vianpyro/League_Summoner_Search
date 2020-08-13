f549bfac2c2b0c254dd1fb5e469b383bdd462d73
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-08-13 17:03:08 -0400

    Improved the champions JSON generator
    Now generates the passive, q, w, e, r, name and description

--- champions_json_generator.py
+++ champions_json_generator.py
@@ -21,9 +21,34 @@ class GetMetaBot:
         return patch[-1][19:25], champions_list, winrate_list
 
 
+class GetStatsBot:
+    def __init__(self):
+        self.base_url = 'https://na.leagueoflegends.com/en-us/champions'
+        
+    def get_champ_stats(self, champion):
+        page = requests.get(f'{self.base_url}/{champion.lower()}/')
+        tree = html.fromstring(page.content)
+
+        try:
+            return (
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[1]/h5/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[1]/p/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[2]/h5/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[2]/p/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[3]/h5/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[3]/p/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[4]/h5/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[4]/p/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[5]/h5/text()')[0].replace("\n", " "),
+                tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/ol/li[5]/p/text()')[0].replace("\n", " "),
+            )
+        except: pass
+
 if __name__ == '__main__':
     bot = GetMetaBot()
     PATCH, CHAMPS, WINRATE = bot.get_meta()
+
+champion_ability = GetStatsBot()
 
 new_champs, new_winrate = [], []
 for i in range (len(CHAMPS)):
@@ -83,32 +108,11 @@ unkown_tier = (
 # Regroup and sort every champion in a single list
 champions = sorted(s_tier + a_tier + b_tier + c_tier + d_tier + e_tier + unkown_tier)
 
-abilities = {
-    'Aatrox': (
-        'DEATHBRINGER STANCE', f'Periodically, Aatrox\'s next basic attack deals bonus physical damage and heals him, based on the target\'s max health.',
-        'THE DARIN BLADE', f'Aatrox slams his greatsword down, dealing 10/30/50/70/90 (+60%) physical damage. He can swing three times, each with a different area of effect.',
-        'INFERNAL CHAINS', f'Aatrox smashes the ground, dealing 30/40/50/60/70 (+40%) physical damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.',
-        'UMBRAL DASH', f'Passively, Aatrox heals 20%/22.5%/25%/27.5%/30% of damage he deals to enemy champions. On activation, he dashes in a direction.',
-        'WORLD ENDER', f'Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining 20%/25%/30% attack damage, and 50%/60%/70% increased healing, and 60%/80%/100% movement speed. If he gets a takedown, this effect is extended.'
-    ),
-    'Ahri': (
-        'VASTAYAN GRACE', f'Whenever Ahri\'s spells hit a champion 2 times within a short period, she briefly gains movement speed.',
-        'ORB OF DECEPTION', f'Ahri sends out and pulls back her orb, dealing 40/65/90/115/140 (+35% AP) magic damage on the way out and 40/65/90/115/140 (+35% AP) true damage on the way back. After earning several spells hits, Ahri\'s next orb hits will restore 3/5/9/18 (+9% AP) of her health.',
-        'FOX-FIRE', f'Releases fox-fires that seek nearby enemies and deal 40/65/90/115/140 (+30% AP) magic damage. Enemies hit with multiple fox-fires take 30% damage from each additional fox-fire beyond the first, for a maximum of ? damage to a single enemy.Fox-fire prioritizes champions recently hit by Charm, then enemies Ahri recently attacked. If Fox-fire cannot find a priority target, it targets champions over the nearest enemy if possible.',
-        'CHARM', f'Ahri blows a kiss that deals 60/90/120/150/180 (+40% AP) magic damage and charms an enemy it encounters, instantly stopping movement abilities and causing them to walk harmlessly towards her. The target temporarily takes increased damage from Ahri.',
-        'SPIRIT RUSH', f'Ahri dashes forward and fires essence bolts that deal 60/90/120 (+35% AP), damaging nearby enemies. Spirit Rush can be cast up to three times before going on cooldown'
-    ),
-    'Yone': (
-        'WAY OF THE HUNTER', f'Yone uses two blades, causing every second Attack to deal 50% magic damage. Yone\'s Critical Strike Chance is doubled but his critical strikes deal 10% reduced damage.',
-        'MORTAL STEEL', f'Yone thrusts forward, dealing 20/45/70/95/120 (+100% AD) physical damage. On hit, grants a stack of for 6 seconds. At 2 stacks, this skill causes Yone to dash forward with a wave of wind that Knocks Up for 0.75 second and deal 20/45/70/95/120 (+100% AD) physical damage.',
-        'SPIRIT CLEAVE', f'Yone cleaves forward in a massive arc, dealing 5/10/15/20/25 + 6%/6.5%/7%/7.5%/8% physical damage and 5/10/15/20/25 + 6%/6.5%/7%/7.5%/8% magical damage of the target\'s maximum health. After successfully hitting an enemy, Yone also gains 40 (+24% AD) shield for 1.5 second. The shield\'s power increases per champion struck.',
-        'SOUL UNBOUND', f'Yone can enter his Spirit Form for 5 seconds, gaining 10% movement speed and leaving his body behind. When Yone\'s Spirit Form expires, he\'ll snap back into his body and deal 25%/27.5%/30%/32.5%/35% of all the damage he dealt with while in Spirit Form.',
-        'FATE SEALED', f'Yone strikes all enemies in his path for 100 (+40% AD) pysical damage and 100 (+40% AD) magic damage, blinking behind the last enemy hit and Knocking Up victims towards him.'
-    )
-}
-
 word = '{\n"champions":\n['
 for index, champ in enumerate(champions):
+    #print champ stats
+    clean_champ_name = champ.replace("'", '-').replace(".", '').replace(" ", '-')
+
     # Don't put a ',' if first element when opening champion part
     if index == 0: word += '{\n'
     else: word += ',\n{\n'
@@ -149,17 +153,18 @@ for index, champ in enumerate(champions):
     word += '\n'
 
     # Add abilities
-    if champ in abilities and len(abilities[champ]) == 10:
-        word += ',"p_name": "%s"\n' % (abilities[champ][0].replace("'", "\\\\'"))
-        word += ',"p_description": "%s"\n' % (abilities[champ][1].replace("'", "\\\\'"))
-        word += ',"q_name": "%s"\n' % (abilities[champ][2].replace("'", "\\\\'"))
-        word += ',"q_description": "%s"\n' % (abilities[champ][3].replace("'", "\\\\'"))
-        word += ',"w_name": "%s"\n' % (abilities[champ][4].replace("'", "\\\\'"))
-        word += ',"w_description": "%s"\n' % (abilities[champ][5].replace("'", "\\\\'"))
-        word += ',"e_name": "%s"\n' %(abilities[champ][6].replace("'", "\\\\'"))
-        word += ',"e_description": "%s"\n' %(abilities[champ][7].replace("'", "\\\\'"))
-        word += ',"r_name": "%s"\n' %(abilities[champ][8].replace("'", "\\\\'"))
-        word += ',"r_description": "%s"\n' %(abilities[champ][9].replace("'", "\\\\'"))
+    champions_abilities = champion_ability.get_champ_stats(clean_champ_name)
+    if champions_abilities != None:
+        word += f',"p_name": "{champions_abilities[0]}"\n'
+        word += f',"p_description": "{champions_abilities[1]}"\n'
+        word += f',"q_name": "{champions_abilities[2]}"\n'
+        word += f',"q_description": "{champions_abilities[3]}"\n'
+        word += f',"w_name": "{champions_abilities[4]}"\n'
+        word += f',"w_description": "{champions_abilities[5]}"\n'
+        word += f',"e_name": "{champions_abilities[6]}"\n'
+        word += f',"e_description": "{champions_abilities[7]}"\n'
+        word += f',"r_name": "{champions_abilities[8]}"\n'
+        word += f',"r_description": "{champions_abilities[9]}"\n'
 
     # Close champion part
     word += '}'
@@ -167,3 +172,5 @@ for index, champ in enumerate(champions):
 word += ']}'
 with open('src/json/champions.json', 'w') as f:
     f.write(word)
+
+input('JSON file generated successfuly, press enter to close this dialog...')
