40117d12498f2da976e994b12a11e82e892f4b2b
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-08-17 13:13:46 -0400

    Made the modal close itself if the user clicks out of it

--- src/javascript/display_champions.js
+++ src/javascript/display_champions.js
@@ -1,16 +1,16 @@
 async function displayChampions() {
     try {
-        //Retrieve champions data from json
+        // Retrieve champions data from json
         const data = new Request(`../src/json/champions.json`);
         const { champions } = await fetch(data).then(data => data.json());
 
-        //Set header text
+        // Set header text
         document.getElementById("champ-toptext").innerHTML = `With ${champions.length} champions, you’ll find the perfect match for your playstyle. Master one, or master them all.`;
 
-        //Create a grid with all the champions
+        // Create a grid with all the champions
         champions.slice().forEach((champion) => {
 
-            //Grid
+            // Grid
             document.getElementById(`champions`).innerHTML += 
             `
             <figure class="champion-figure">
@@ -21,7 +21,7 @@ async function displayChampions() {
             </figure>
             `;
 
-            //Display champ data when clicked on
+            // Display champ data when clicked on
             document.querySelectorAll(".championData").forEach(e => {
                 e.addEventListener("click", () => {
                     loadChampData(e.dataset.champion)
@@ -32,15 +32,22 @@ async function displayChampions() {
             document.getElementById(`champ${champion.name}Name`).innerHTML = name;
         });        
 
-        //Close modal by clicking on modal-close
+        // Close modal by clicking on modal-close
         document.getElementById('modal-close').addEventListener('click', () => document.getElementById('modal').style.display = 'none');
         
-        //Close modal by pressing esc key
+        // Close modal by pressing esc key
         document.body.onkeyup = e => {
             if (e.key === 'Escape' || e.key === 'Esc') return document.getElementById('modal').style.display = 'none';
         }
 
-        //if champs img not found
+        // Close modal by clicking out of it
+        window.addEventListener('click', function (e) {
+            if (!document.getElementById('modal-wrapper').contains(e.target)) {
+                return document.getElementById('modal').style.display = 'none';
+            }
+        });
+
+        // If champs img not found
         document.querySelectorAll('img').forEach(img => img.onerror = function () { this.src = '../src/img/image_not_found.png'; });
 
     } catch (err) {
